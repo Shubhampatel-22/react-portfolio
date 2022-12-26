0a1595820bd01f7553bd91868b4d31c586e08431
Shubhampatel-22 <spatel2262001@gmail.com>
2022-12-26 20:00:51 +0530

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div name="about" className=' w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto     flex flex-col justify-center w-full h-full '>
+            <div className='pb-8'>
+                <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className=' text-xl mt-20'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aspernatur aliquam tempore dignissimos debitis voluptatem sapiente dolore sit, repellendus quo natus? Eos veritatis similique eaque dolore? Quisquam facere ullam, error quo laboriosam corrupti quas necessitatibus doloremque ut veritatis ratione expedita inventore omnis odio optio? At pariatur placeat molestias deserunt atque.
+            </p>
+            <br/>
+            <p className=' text-xl'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque beatae perspiciatis fugiat, quas, officia porro itaque veritatis blanditiis ducimus debitis suscipit harum delectus qui officiis facilis dolore veniam laudantium ex.
+            </p>
+    </div>
+    </div>
+  )
+}
+
+export default About
